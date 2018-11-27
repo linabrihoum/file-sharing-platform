9f61527bd6900433f1aa94e6abd59bf00b7333a7
Diego De La Torre <diegodltl@gmail.com>
2018-11-27 02:08:27 +0000

    add 'project dropdown' container and 'file card' component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from'./App.css';
+
 import AuthForm from './containers/AuthForm/AuthForm';
+import Projects from './containers/Projects/Projects';
+import FileCard from './components/FileCard/FileCard'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <h1>Home</h1>
-       <AuthForm />
+         <div className={classes.header}>
+            <Projects />
+         </div>
+         <div className={classes.body}>
+            <div className={classes.sideBar}>
+            </div>
+            <div>
+              <FileCard />
+              <FileCard />
+              <FileCard />
+              <FileCard />
+              <FileCard />
+              <FileCard />
+              <FileCard />
+              <FileCard />
+              <FileCard />
+              <FileCard />
+              <FileCard />
+            </div>
+         </div>
       </div>
     );
   }
