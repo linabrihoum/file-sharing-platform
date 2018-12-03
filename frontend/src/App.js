1cf896aa34f1d21e02354bbe1955e65c67bad9e4
Diegodlt <diegodltl@gmail.com>
2018-12-02 22:38:26 -0500

    build layout with reactstrap components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import classes from'./App.css';
-import { Input,InputGroup, Button} from 'reactstrap';
+import { Navbar, Row, Col} from 'reactstrap';
 import axios from 'axios';
 
 import AuthForm from './containers/AuthForm/AuthForm';
@@ -14,12 +14,23 @@ class App extends Component {
   state = {
     projects: null,
     currentProject:null,
-    currentFiles: []
+    currentFiles: [],
+    windowWidth: window.innerWidth,
+    windowHeight: window.innerHeight
   }
-  
+
+  resize(){
+    // This will determine the height and width of the window
+    this.setState({
+      windowHeight: window.innerHeight,
+      windowWidth: window.innerWidth
+    })
+  }
+
   componentDidMount(){
-        console.log("Component did mount from App.js");
-        
+    // This attaches an event listener on the window 
+    this._resize = this.resize.bind(this);
+    window.addEventListener("resize", this._resize);
         axios.get("https://react-my-burger-dfbcb.firebaseio.com/projects.json")
             .then(response =>{
                 this.setState({projects: response.data});
@@ -28,13 +39,6 @@ class App extends Component {
             })
     }
     
-    
-     componentWillUpdate(){
-      console.log("componentDidUpdate from App.js");
-      
-    }
-  
-  
   setCurrentProject(project){
     console.log("Set Project:",project);
         this.setState({currentProject: project});
@@ -54,36 +58,48 @@ class App extends Component {
   
   render() {
     
-    console.log("render from App.js")
-    console.log(this.state.currentFiles);
+    const style = {
+      height: this.state.windowHeight - 54,
+      padding: 0,
+      width: this.state.windowWidth,
+      backgroundColor: "gray",
+      margin:0,
+      border: "2px solid green"
+    }
+    
     let files = this.state.currentFiles.map((file)=>
       <FileCard project = {file} />
     )
-    console.log(files);
     
     return (
-      <div className="App">
-      
-         <div className={classes.header}>
-            <Projects 
-            projects = { this.state.projects }
-            setProject = {this.setCurrentProject.bind(this)}/>
-         </div>
-         
-         <div className={classes.body}>
-            <div className={classes.sideBar}>
-            </div>
-          
-            <div>
-              <div className={classes.innerBar}>
-                <Input className={classes.search} placeholder ="test"/>
-              </div>
-              <div className={classes.fileCards}>
-                  {files}
-              </div>
-            </div>
-         </div>
-      </div>
+      <div className={classes.App}>
+        <Navbar color="dark">
+          <Projects />
+        </Navbar>
+        <Row style={style}>
+            <Col xs = "2" style={{"background-color" : "rgba(0,0,255, 0.5)"}}>SideBar</Col>
+              <Col xs="10" style ={{"background-color" : "rgba(0, 255, 0, 0.5)"}}>
+                <Row>
+                  <Col style = {{"background-color" : "rgba(255,0,0,1)", "padding":"10px"}}>innercol</Col>
+                </Row>
+                <Row>
+                  <Col style = {{"background-color": "white", "height" : style.height - 48, "overflow": "auto"}}>
+                    <img style={{"padding": "10px"}}src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"/>
+                    <img style={{"padding": "10px"}}src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"/>
+                    <img style={{"padding": "10px"}}src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"/>
+                    <img style={{"padding": "10px"}}src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"/>
+                    <img style={{"padding": "10px"}}src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"/>
+                    <img style={{"padding": "10px"}}src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"/>
+                    <img style={{"padding": "10px"}}src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"/>
+                    <img style={{"padding": "10px"}}src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"/>
+                    <img style={{"padding": "10px"}}src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"/>
+                    <img style={{"padding": "10px"}}src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"/>
+                    <img style={{"padding": "10px"}}src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"/>
+                  </Col>
+                </Row>
+               </Col>
+         </Row>
+       </div>
     );
   }
 }
