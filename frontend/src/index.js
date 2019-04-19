73589a38eaead04d13c1353732b0505a191f8a08
Diego <diegodltl@gmail.com>
2019-04-19 10:37:56 -0400

    Integrating index.js file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
+import io from 'socket.io-client';
+import SocketIOFileClient from 'socket.io-file-client';
+import Crypt from 'g-crypt';
 
 import './index.css';
 import App from './App';
@@ -13,6 +16,24 @@ const rootReducer = combineReducers({
 })
 
 const store = createStore(rootReducer);
+
+// Define encryption passphrase.
+var passphrase = 'Qpud>CdkUbtu^yQ;!a>Ja`Zv?szt<22v';
+
+// Define socket connection.
+window.socket = io('https://zach.black:3001', {secure: true});
+//window.socket = io('http://45.79.74.60:3001', {secure: true});
+
+// Uploader.
+window.uploader = new SocketIOFileClient(window.socket);
+
+// Define crypter.
+window.crypter = Crypt(passphrase);
+
+// Wait for connection to server.
+window.socket.on('connect', () => {
+    console.log('Connected to server.');
+});
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
