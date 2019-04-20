5c9fc4064741e6faae2d9dd038a7430509c97b9b
Diego <diegodltl@gmail.com>
2019-04-20 10:48:00 -0400

    Integrate Project Drop Down component

--- frontend/src/containers/Projects/Projects.js
+++ frontend/src/containers/Projects/Projects.js
@@ -1,53 +1,103 @@
 import React, { Component } from 'react';
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
-import axios from 'axios';
+import ProjectDropdownItem from './ProjectDropDownItem/ProjectDropDownItem'
 
-import classes from './Projects.css'
+export default class ProjectDropdown extends Component {
+  constructor(props) {
+    super(props);
 
-class Projects extends Component{
-    
-    state = {
-        project: "Select Project",
-        dropdownOpen: false,
-        projects : null
-    }
-    
-    toggle(){
-        this.setState(prevState =>({
-            dropdownOpen : !prevState.dropdownOpen
-        }));
-    }
-    
-    render(){
-        
-        let dropDowns = <DropdownItem>Projects Loading...</DropdownItem>;
-        if(this.props.projects){
-             dropDowns = this.props.projects.map((project)=>
-                <DropdownItem
-                    onClick ={()=>{
-                        this.setState({project: project});
-                        this.props.setProject(project);
-                        }
-                    }
-                >{project}
-                </DropdownItem>
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      projectName: 'Select Project',
+      currentProject: null,
+      availableProjects: [],
+      dropdownOpen: false
+    };
+
+    // Bind methods.
+    this.setCurrentProject = this.setCurrentProject.bind(this);
+    this.toggle = this.toggle.bind(this);
+    this.isProjectSelected = this.isProjectSelected.bind(this);
+
+  }
+
+  componentDidMount() {
+    window.socket.emit('request_projectList', (encrypted_data) => {
+      // Decrypt message from server.
+      var data = window.crypter.decrypt(encrypted_data);
+
+      // Update available projects.
+      this.setState({ availableProjects: data });
+    });
+  }
+
+  toggle() {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen
+    }));
+  }
+
+  setCurrentProject(key) {
+    /* GET THE PROJECT TREE*/
+    // Let server know what project is selected.
+    window.socket.emit('request_project',
+      window.crypter.encrypt({ ID: this.state.availableProjects[key].ID }),
+      (encryptedProjectData) => {
+
+        // Get the project tree.
+        var projectTree = window.crypter.decrypt(encryptedProjectData);
+
+        console.log(projectTree);
+
+        //projectTree.contents['data.csv'].hash
+      });
+      /* -------------------------- */
+
+      /* FILE DOWNLOAD EXAMPLE
+
+       window.socket.emit('request_download',
+          window.crypter.encrypt({ Hash: "PUT HASH HERE"}),
+          (encryptedDownloadToken) => {
+
+            // Get the project tree.
+            var downloadToken = window.crypter.decrypt(encryptedDownloadToken);
+
+            // DOWNLOAD URL: https://zach.black:3001/download/?token=<TOKEN HERE>
+            console.log(downloadToken);
+          });
+      */
+
+    // Update project selection state.
+    this.setState({ projectName: this.state.availableProjects[key].Name, currentProject: key });
+    console.log('You have selected project ' + this.state.availableProjects[key].ID);
+  }
+
+  isProjectSelected() {
+    return this.state.currentProject != null;
+  }
+
+  render() {
+    return (
+      <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+        <DropdownToggle caret>
+          {this.state.projectName}
+        </DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem header>Available Projects</DropdownItem>
+          {this.state.availableProjects.map((project, index) => {
+            return (
+              <ProjectDropdownItem
+                key={index}
+                projectKey={index}
+                setProject={this.setCurrentProject}
+                disabled={!project.Access}
+              >
+                {project.Name}
+              </ProjectDropdownItem>
             );
-        }
-        
-        
-        return(
-            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle.bind(this)}>
-                <DropdownToggle 
-                    className={classes.dropDown} caret>
-                    {this.state.project}
-                </DropdownToggle>
-                <DropdownMenu>
-                   {dropDowns}
-                </DropdownMenu>
-            </Dropdown>
-            
-        );
-    }
+          })}
+        </DropdownMenu>
+      </Dropdown>
+    );
+  }
 }
-
-export default Projects;
